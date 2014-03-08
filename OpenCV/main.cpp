f36c9a8e4b18d1dd1ceb91ff0e02a22cbee29a71
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-03-08 00:37:50 -0500

    Added Windows Form as UI

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -11,17 +11,29 @@
 */
 #include "MotorController.h"
 #include "ImageProcessor.h"
+#include "RobotControlUI.h"
 
 using namespace cv;
 
+using namespace System;
+using namespace System::Windows::Forms;
+
 int main(int argc, char* argv[])
 {
-	
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+	TennisBallCollection::RobotControlUI form;
+    Application::Run(%form);
+
+	/*
+		
 	MotorController mC;
 	mC.start("\\\\.\\COM17");
 
 	ImageProcessor ip(mC);
 	ip.process();
+	*/
 
 	return 0;
 }
