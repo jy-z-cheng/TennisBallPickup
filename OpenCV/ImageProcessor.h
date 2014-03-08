a2c7999bb95d6db32a4df4e204f1833ced9f6aa3
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-03-07 21:48:53 -0500

    Modularized image processing to separate classes

---
+++ OpenCV/ImageProcessor.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "TennisBall.h"
+#include <opencv\highgui.h>
+#include <opencv\cv.h>
+#include "MotorController.h";
+
+using namespace std;
+using namespace cv;
+
+class ImageProcessor
+{
+public:
+	ImageProcessor(void);
+	~ImageProcessor(void);
+	void process();
+
+private:
+	MotorController motorController;
+
+	//void on_trackbar( int, void* );
+	string intToString(int number);
+	void createTrackbars();
+	void drawObject(vector<TennisBall> theTennisBalls,Mat &frame);
+	void morphOps(Mat &thresh);
+	int checkPosition(bool haveBall, TennisBall tennisBall);
+	void trackFilteredObject(Mat threshold,Mat HSV, Mat &cameraFeed);
+
+};
