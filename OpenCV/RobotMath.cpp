76a85c2681a68072d0eebf2966f5d978a429027d
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-05-25 16:41:36 -0400

    Created RobotMath namespace
    To be used for containing the functions that does all the math

---
+++ OpenCV/RobotMath.cpp
@@ -0,0 +1,19 @@
+#include "RobotMath.h"
+
+
+namespace utility
+{
+
+	void findSlopeOfLine() 
+	{
+		//code
+	}
+	void function2() 
+	{
+		//code
+	}
+	void function3() 
+	{
+		//code
+	}
+}
---
+++ OpenCV/RobotMath.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+namespace RobotMath
+{
+   void findSlopeOfLine();
+   void function2();
+   void function3();
+
+   template<typename T>
+   void function4()
+   {
+      //function template definition should be here!
+   }
+}
+
